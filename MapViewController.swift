d44a2ee7cfeb8ef9338a40b5aeb638e0488de3b3
aiolos404 <aiolos404@gmail.com>
2015-02-11 09:32:23 -0600

    Add delegate to the map view

--- MapViewController.swift
+++ MapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  EatAround
 //
 //  Created by aiolos404 on 2/10/15.
@@ -8,21 +8,53 @@
 
 import UIKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController,GMSMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var camera = GMSCameraPosition.cameraWithLatitude(-33.86,
-            longitude: 151.20, zoom: 6)
-        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+//        var camera = GMSCameraPosition.cameraWithLatitude(-33.86, longitude: 151.20, zoom: 6)
+//        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+//        mapView.myLocationEnabled = true
+//        mapView.mapType = kGMSTypeHybrid
+//        
+////        var mapInsets = UIEdgeInsetsMake(300.0,300.0, 300.0, 300.0)
+////        mapView.padding = mapInsets
+////        
+//        
+//        self.view = mapView
+//        
+//        var marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+//        marker.title = "Chicago"
+//        marker.snippet = "American"
+//        marker.map = mapView
+    }
+    
+    
+    override func loadView() {
+        var camera = GMSCameraPosition.cameraWithLatitude(41.8760,
+            longitude:-87.6266, zoom:11)
+        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
+       
+        mapView.delegate = self
+        
         mapView.myLocationEnabled = true
+        mapView.mapType = kGMSTypeHybrid
+        
         self.view = mapView
+        
         
         var marker = GMSMarker()
         marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.title = "Chicago"
+        marker.snippet = "American"
         marker.map = mapView
+    }
+    
+    // MARK: GMSMapViewDelegate
+    
+    func mapView(mapView: GMSMapView!, didTapAtCoordinate coordinate: CLLocationCoordinate2D) {
+        NSLog("You tapped at %f,%f", coordinate.latitude, coordinate.longitude)
     }
 }
